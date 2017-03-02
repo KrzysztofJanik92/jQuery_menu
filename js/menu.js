81c4cb06b259248270bd6cf9d82a66ec7cbf1a62
KrzysztofJanik92 <krzysztof.janik@vp.pl>
2017-03-02 10:38:13 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -5,11 +5,6 @@ var main = function() {
       left: "0px"
     }, 300);
 	$(".xyz").hide(300);
-	
-/*Push the body by 200px*/
-    /*$('body').animate({
-      left: "200px"
-    }, 300);*/
   }); 
   
    /* Then push them back nav*/
@@ -18,11 +13,6 @@ var main = function() {
       left: "-200px"
     }, 300);
 	$(".xyz").show(300);
-
-	/*Then push them back body*/
-    /*$('body').animate({
-      left: "0px"
-    }, 300);*/
   });
 };
 
